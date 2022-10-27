cc907f617278d1e01c74a813c5d830499845d10b
klebiedzinski <lebiedzinskik02@gmail.com>
2022-10-27 16:18:19 +0200

    basic layout

--- lab04/src/Components/ProductsList.js
+++ lab04/src/Components/ProductsList.js
@@ -1,28 +1,43 @@
 import axios from 'axios';
 import {useEffect, useState} from 'react';
+const _ = require("lodash")
+
 const ProductsList = () => {
     const [products, setProducts] = useState([]);
+    const [categories, setCategories] = useState([])
     useEffect(() => {
         axios
             .get('https://fakestoreapi.com/products/')
             .then(res => {
                 const products_with_id = res.data
                 setProducts(products_with_id);
-                console.log(products)
+                const newCategories = _uniqBy(products, "category")
+                console.log(newCategories)
             })
         
     },[])
     return ( 
+        <div className="productsContainer">
+            <div className="productsCategories">
+                {products.map(product => (
+                    <div className="category">
+                        <h3>{product.category}</h3>
+                    </div>
+                ))}
+            </div>
         <div className="productsList">
             {products.map(product => (
                 <div className="product" key={product.id}>
-                    <h3>{product.title}</h3>
-                    <h5>{product.category}</h5>
+                    <img src={product.image} alt="product-img"></img>
+                    <div className="productInfo">
+                    <h1>{product.title}</h1>
+                    <h2>{product.category}</h2>
                     <h5>{product.price}</h5>
-                    <img src={product.image} alt="image"></img>
+                    </div>
                 </div>
             ))}
         </div> 
+        </div>
         );
 }
  
